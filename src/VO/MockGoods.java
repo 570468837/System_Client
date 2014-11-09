16bc9f29d1723dffd9e43dc3838b91163768c32c
Bagnogetoff <1051943988@qq.com>
2014-11-09 16:47:50 +0800

    mockgoods

---
+++ src/VO/MockGoods.java
@@ -0,0 +1,18 @@
+package VO;
+/**
+ * 
+ * @author hutao
+ * 类协作的桩程序
+ */
+public class MockGoods extends GoodsVO{
+	long price;
+	public MockGoods(long price) {
+		this.price = price;
+	}
+	
+	public long getPrice() {
+		return price;
+	}
+	
+	
+}
