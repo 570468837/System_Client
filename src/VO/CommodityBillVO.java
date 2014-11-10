28caa98e5ffb9408dec1ed09bab7052ddaa6590a
Bagnogetoff <1051943988@qq.com>
2014-11-10 17:17:40 +0800

    同PO

--- src/VO/CommodityBillVO.java
+++ src/VO/CommodityBillVO.java
@@ -2,8 +2,8 @@ package VO;
 /**
  * 
  * @author hutao
- * 库存类据单，即报单和赠送单的父类
+ * 库存类据单，即报单和赠送单的父类，用于过滤掉接收单据中与库存类据单差距较大的属性
  */
-public abstract class CommodityBillVO {
+public abstract class CommodityBillVO extends ReceiptVO {
 
 }
