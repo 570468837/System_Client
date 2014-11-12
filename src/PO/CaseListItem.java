45babfbf2f75f0cc37d162f5cc8c438d683b763d
WRKMNO4 <15850592396@163.com>
2014-11-12 17:29:02 +0800

    原先的目录清单构造器重复，去掉一个

--- src/PO/CaseListItem.java
+++ src/PO/CaseListItem.java
@@ -6,12 +6,6 @@ public class CaseListItem {
 	double caseMoney ; //金额
 	String remark;// 备注
 	
-	public CaseListItem(String casename,double caseMoney,String remark){
-		this.casename=casename;
-		this.caseMoney=caseMoney;
-		this.remark=remark;
-	}
-	
 	public CaseListItem(String casename, double caseMoney, String remark) {
 		this.casename = casename;
 		this.caseMoney = caseMoney;
