ef866514da7db93cf4bb7599dfc4be132a218b70
Bagnogetoff <1051943988@qq.com>
2014-11-23 19:43:19 +0800

    =，=

--- src/VO/InventoryCommodityVO.java
+++ src/VO/InventoryCommodityVO.java
@@ -5,7 +5,7 @@ package VO;
  *
  */
 public class InventoryCommodityVO {
-	
+	public String[][] icInfo = {{"1", "a", "a", "a", "a", "a", "a", "a"}};
 	
 	public void print() {
 		System.out.println("<库存盘点信息>");
--- src/presentation/CommodityFrame.java
+++ src/presentation/CommodityFrame.java
@@ -4,6 +4,9 @@ import java.awt.*;
 import java.awt.event.*;
 
 import javax.swing.*;
+
+import businesslogicservice.CommodityBLService.CommodityController;
+import businesslogicservice.GoodsBLService.GoodsController;
 /**
  * 
  * @author hutao
@@ -102,46 +105,135 @@ public class CommodityFrame extends JFrame {
 	
 	
 	class GoodsPanel extends JPanel {
-		//private JTable goodsTable;
+		private JScrollPane jsp;
+		private GoodsController gc = new GoodsController();
+		private JTable goodsTable;
+		private JLabel searchLabel = new JLabel("搜索", JLabel.CENTER);
+		private JTextField searchField = new JTextField("<请输入商品关键字>");
+		
 		public GoodsPanel(JFrame theFrame) {
 			super();
-			
 			this.setBounds(140, 25, 835, 550);
 			this.setBackground(new Color(150, 255, 150, 255));
+			this.setLayout(null);
+			
+			searchField.setBounds(310, 20, 200, 25);
+			searchField.addFocusListener(new FocusListener() {
+		    	public void focusGained(FocusEvent e) {
+		    		if(searchField.getText().equals("<请输入商品关键字>")) {
+		    			searchField.setText("");
+		    		}
+		    	}
+	            public void focusLost(FocusEvent e) {
+		    		if(searchField.getText().equals("")) {
+		    			searchField.setText("<请输入商品关键字>");
+		    		}
+		    	}
+		    });
+			
+			
+			searchLabel.setFont(new Font("default", 1, 18));
+			searchLabel.setBounds(530, 19, 60, 25);
+			searchLabel.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseClicked(MouseEvent event) {
+					
+				}
+		});
 			
 			
 			
+			
+			
+			
+			
+			
+			
+			
+			this.add(searchField);
+			this.add(searchLabel);
+			//this.add(goodsTable); 
 			theFrame.add(this);
 		}
+		/**
+		 * 
+		 * @return 获取所有商品信息
+		 */
+		private String[][] getTableInfo() {
+			
+			return null;
+		}
+		
 	}
 	
 	class CommodityPanel extends JPanel {
-		private JTable comTable;
+		private JScrollPane jsp;
+		private JTable comCheckTable;
+		private JTable comInvenTable;
+		private CommodityController cc = new CommodityController();
+		private JLabel
+		    comCheckLabel,
+		    comInvenLabel,
+		    sendLabel,
+		    reportLabel,
+		    exportLabel;
+		    
+		
 		public CommodityPanel(JFrame theFrame) {
 			super();
 			
 			this.setBounds(140, 25, 835, 550);
 			this.setBackground(new Color(170, 255, 170, 255));
+			this.setLayout(null);
+			
+			
+			comInvenLabel = new JLabel("库存盘点");
+			
+			comCheckLabel = new JLabel("库存查看");
+			
+			sendLabel = new JLabel("制定赠送单");
+			
+			reportLabel = new JLabel("制定报单");
+			
+			/*
+			 * 初始化显示库存盘点界面
+			 */
+			String[] citHead = {"", "名称", "型号", "库存数量", "库存均价", "批次", "批号", "出厂日期"};
+			String[][] citInfo = cc.inventoryCommodity().icInfo;
+			comInvenTable = new JTable(citInfo, citHead);
+			comInvenTable.setAutoResizeMode(0);
+			comInvenTable.setRowHeight(25);
+			comInvenTable.setPreferredSize(new Dimension(825, citInfo.length * 25));
+			comInvenTable.setEnabled(false);
+			comInvenTable.setFont(new Font("default", 0, 16));
+			comInvenTable.getTableHeader().setReorderingAllowed(false);
+			comInvenTable.getTableHeader().setEnabled(false);
+			comInvenTable.getTableHeader().setFont(new Font("default", 1, 17));
+			comInvenTable.getTableHeader().setPreferredSize(new Dimension(0, 35));
+			comInvenTable.getColumnModel().getColumn(0).setPreferredWidth(30);
+			jsp = new JScrollPane(comInvenTable);
+	    	jsp.setHorizontalScrollBar(null);
+			jsp.setBounds(5, 100, 825, 400);
+			/*
+			 * 导出excel键
+			 */
+			exportLabel = new JLabel("export");
+			exportLabel.setBounds(700, 500, 100, 50);
+			exportLabel.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseClicked(MouseEvent event) {}
+			});
 			
 			
 			
-			
-			
-			
-			
-			
-			
+			this.add(jsp);
+			this.add(exportLabel);
 			theFrame.add(this);
 		}
 		
-		private String[][] getTableInfo() {
-			return null;
-		}
-		
-		
 	}
-	/*
-	 * 报警单区域
+	/**
+	 * 报警单区域，包括报警单和总经理的回执
 	 */
 	class AlarmFrame extends JFrame {
 		JLabel exitLabel;
