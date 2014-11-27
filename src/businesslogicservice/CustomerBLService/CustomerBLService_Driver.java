7de6184290113debf7fd0aae6e3e383f99499cfb
gaoyangyiqiao <gyyq1994@gmail.com>
2014-11-27 10:17:40 +0800

    部分逻辑

--- src/businesslogicservice/CustomerBLService/CustomerBLService_Driver.java
+++ src/businesslogicservice/CustomerBLService/CustomerBLService_Driver.java
@@ -1,13 +1,19 @@
 package businesslogicservice.CustomerBLService;
 
+import Config.Level;
+import Config.Sort;
+import ResultMessage.ResultMessage;
 import VO.CustomerVO;
 
 public class CustomerBLService_Driver {
 	public CustomerBLService_Driver(CustomerBLService cbs){
-		cbs.addCustomer(new CustomerVO());
-		cbs.deleteCustomer(new CustomerVO());
-		cbs.updateCustmer(new CustomerVO());
-		cbs.findCustomer("",0);		
+		ResultMessage result;
+		
+		CustomerVO vo=new CustomerVO("001", Sort.importer, Level.fiveClassVIP, "sue","110","there","00123", "@", "qiao");
+		System.out.println(cbs.addCustomer(new CustomerVO()));
+//		cbs.deleteCustomer(new CustomerVO());
+//		cbs.updateCustmer(new CustomerVO());
+//		cbs.findCustomer("",0);		
 	}
 	
 	
