ecf583eb2fd48ae608093f0e9f2fd540c7705e5c
gaoyangyiqiao <gyyq1994@gmail.com>
2014-11-30 20:53:55 +0800

    .......

--- src/businesslogicservice/CustomerBLService/CustomerBLService_Driver.java
+++ src/businesslogicservice/CustomerBLService/CustomerBLService_Driver.java
@@ -6,11 +6,12 @@ import ResultMessage.ResultMessage;
 import VO.CustomerVO;
 
 public class CustomerBLService_Driver {
-	public CustomerBLService_Driver(CustomerBLService cbs){
+	public CustomerBLService_Driver(CustomerController cbs){
 		ResultMessage result;
 		
-		CustomerVO vo=new CustomerVO("001", Sort.importer, Level.fiveClassVIP, "sue","110","there","00123", "@", "qiao");
-		System.out.println(cbs.addCustomer(new CustomerVO()));
+		CustomerVO vo=new CustomerVO("003", Sort.importer, Level.fiveClassVIP, "kue","110","there","00123", "@", "qiao");
+		cbs.addCustomer(vo);
+//		System.out.println(cbs.addCustomer(new CustomerVO()));
 //		cbs.deleteCustomer(new CustomerVO());
 //		cbs.updateCustmer(new CustomerVO());
 //		cbs.findCustomer("",0);		
@@ -18,7 +19,7 @@ public class CustomerBLService_Driver {
 	
 	
 	public static void main(String [] args){
-		CustomerBLService customerController=new CustomerController();
+		CustomerController customerController=new CustomerController();
 		new CustomerBLService_Driver(customerController);
 	}
 	
