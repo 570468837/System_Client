1d1e358d2dce1ea5f7fac6fa95e299621fd22c23
Bagnogetoff <1051943988@qq.com>
2014-12-13 13:31:53 +0800

    =，=

--- src/VO/CheckCommodityVO.java
+++ src/VO/CheckCommodityVO.java
@@ -1,6 +1,8 @@
 package VO;
 
-import PO.CheckCommodityPO;
+import java.util.ArrayList;
+
+import PO.PurchaseReceiptPO;
 
 /**
  * 
@@ -8,38 +10,20 @@ import PO.CheckCommodityPO;
  *
  */
 public class CheckCommodityVO {
-	public String time1;
-	public String time2;
-	public String[][] info = {
-			{"a", "10", "20", "销售", "200"},
-			{"b", "10", "20", "进货", "-200"},
-			{"c", "10", "20", "赠送", "-200"},
-			{"合计", "", "", "", "-200"},};
+	public String[][] info;
 	
 	
-	public CheckCommodityVO(String time1, String time2) {
-		this.time1 = time1;
-		this.time2 = time2;
-	}
-	
-	/**
-	 * 将VO转换为PO
-	 * @return 返回转换成的PO
-	 */
-	public CheckCommodityPO toPO() {
-		CheckCommodityPO ccp = new CheckCommodityPO(time1, time2);
-		ccp.info = this.info;
+	@SuppressWarnings({ "unchecked", "unused" })
+	public CheckCommodityVO(Object receipt) {
+		ArrayList<Object> r = (ArrayList<Object>)receipt;
+		ArrayList<PurchaseReceiptPO> purchaseReceipts = (ArrayList<PurchaseReceiptPO>)r.get(0);//进货单
+		ArrayList<PurchaseReceiptPO> purchaseOutReceipts = (ArrayList<PurchaseReceiptPO>)r.get(1);//进货退货单
 		
-		return ccp;
-	}
-	/**
-	 * 将PO转换为VO
-	 * @param po 待转换的PO
-	 */
-	public void toVO(CheckCommodityPO po) {
-		this.time1 = po.time1;
-		this.time2 = po.time2;
-		this.info = po.info;
+		
+		
+		
+		
+		
 	}
 
 }
